48358a0a6356e987ab7c3e71baeda6e4ca98733f
evelynrodriguezc <evelynrodriguezc0@gmail.com>
2022-10-19 23:55:57 -0500

    adding icons and creating a new dates interval

--- src/index.js
+++ src/index.js
@@ -1,72 +1,100 @@
-const months = document.querySelector("#months")
-const weeks = document.querySelector("#weeks")
-const days = document.querySelector("#days")
-const hours = document.querySelector("#hours")
-const minutes = document.querySelector("#minutes")
-const seconds = document.querySelector("#seconds")
+const months = document.querySelector("#months");
+const weeks = document.querySelector("#weeks");
+const days = document.querySelector("#days");
+const hours = document.querySelector("#hours");
+const minutes = document.querySelector("#minutes");
+const seconds = document.querySelector("#seconds");
 
 const today = new Date();
-const christmasYear = today.getFullYear()
+const christmasYear = today.getFullYear();
 
 if(today.getMonth() === 11 && today.getDate() > 25){
-    christmasYear += 1
+    christmasYear += 1;
 }
 
 const christmasDate = new Date(christmasYear, 11, 25);
 const daysMiliseconds = 1000 * 60 * 60 * 24;
 
-const remainingDays = Math.ceil(
+let remainingDays = parseInt(
     (christmasDate.getTime() - today.getTime()) / daysMiliseconds
 );
-
-console.log(remainingDays);
 
 let monthsValue = parseInt(remainingDays / 30);
 months.textContent = monthsValue;
 
-let weeksValue = parseInt(remainingDays % 30);
+remainingDays = parseInt(remainingDays % 30);
+let weeksValue = remainingDays;
 weeksValue = Math.floor(weeksValue / 7);
 weeks.textContent = weeksValue;
-console.log(weeksValue);
 
-
-let daysValue = remainingDays % 30;
+let daysValue = parseInt(remainingDays % 7);
 days.textContent = daysValue;;
 
-let hoursValue = 23;
-let minutesValue = 59;
-let secondsValue = 59;
+let hoursValue = 23 - today.getHours();
+let minutesValue = 60 - today.getMinutes();
+let secondsValue = 60 - today.getSeconds();
 
 hours.textContent = hoursValue
 minutes.textContent = minutesValue
 seconds.textContent = secondsValue
 
- setInterval(() => {
-    if(secondsValue === 0 && minutesValue === 0 && hoursValue === 0 && daysValue === 0 && weeksValue === 0 && monthsValue === 0){
-        alert("It's Xmas!!!");
-    }
-
-    seconds.textContent = secondsValue;
-
-    if(secondsValue === 0){
-        secondsValue = 59;
-        minutesValue -= 1;
-        
-        if(minutesValue === 0){
-            minutesValue = 59;
-            hoursValue -= 1;
-            if(hoursValue === 0){
-                hoursValue = 23;
-                daysValue -= 1;
-            }
-            hours.textContent = hoursValue;
-        }
-        minutes.textContent = minutesValue;
+let Interval = setInterval(() => {
+    if (
+      today.getDay() === christmasDate.getDay() &&
+      today.getMonth() === christmasDate.getMonth()
+    ) {
+      alert("It's Xmas!!! 🎄");
+      months.textContent = 0;
+      weeks.textContent = 0;
+      days.textContent = 0;
+      hours.textContent = 0;
+      minutes.textContent = 0;
+      seconds.textContent = 0;
+      clearInterval(Interval);
     } else {
-        secondsValue -=1;
     }
+  
+    switch (true) {
+      case secondsValue === 0 &&
+        minutesValue === 0 &&
+        hoursValue === 0 &&
+        daysValue === 0 &&
+        weeksValue === 0 &&
+        monthsValue === 0:
+        clearInterval(Interval);
+        return;
+      case secondsValue === 0:
+        secondsValue = 60;
+        minutesValue -= 1;
+        minutes.textContent = minutesValue;
+        break;
+      case minutesValue === 0:
+        minutesValue = 60;
+        hoursValue -= 1;
+        minutes.textContent = minutesValue;
+        hours.textContent = hoursValue;
+        break;
+      case hoursValue === 0:
+        hoursValue = 24;
+        daysValue -= 1;
+        hours.textContent = hoursValue;
+        days.textContent = daysValue;
+        break;
+      case daysValue === 0:
+        daysValue = 6;
+        weeksValue -= 1;
+        days.textContent = daysValue;
+        weeks.textContent = weeksValue;
+        break;
+      case weeksValue === 0:
+        weeksValue = 4;
+        monthsValue -= 1;
+        weeks.textContent = weeksValue;
+        months.textContent = monthsValue;
+        break;
+    }
+  
+    secondsValue -= 1;
     seconds.textContent = secondsValue;
-
-}, 1000);
-
-
+  }, 1000);
+  
